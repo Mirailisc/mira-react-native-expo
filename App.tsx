665d71b9c2dfd99e375141c470b2eb8e0f182c8c
Mirailisc <mirailisclm@gmail.com>
2021-12-20 11:33:33 +0700

    first commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.header}>
+        <Image style={styles.avatar} source={require('./images/avatar.jpeg')} />
+        <Text style={styles.headerText}>Hi, Phubordin</Text>
+      </View>
+      <View style={styles.content}>
+        <Text style={styles.contentHeader}>Todo</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +19,35 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#F05454',
   },
+  header: {
+    flex: 0.25,
+    justifyContent: 'flex-end',
+    paddingLeft: 20,
+    paddingBottom: 10,
+  },
+  headerText: {
+    fontSize: 26,
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  avatar: {
+    width: 90,
+    height: 90,
+    borderRadius: 90 / 2,
+  },
+  content: {
+    backgroundColor: '#F5F5F5',
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
+    flex: 0.75,
+  },
+  contentHeader: {
+    fontSize: 18,
+    marginLeft: 40,
+    marginTop: 20,
+    fontWeight: 'bold',
+    color: '#30475E',
+  }
 });
